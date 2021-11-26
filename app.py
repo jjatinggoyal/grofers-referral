81c53f90c06cbd7332c4bce84021fdeb2cf20f9a
jjatinggoyal <45630976+jjatinggoyal@users.noreply.github.com>
2021-11-27 01:50:14 +0530

    view db

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 @app.route("/admin")
 def hello():
-    # conn = psycopg2.connect(dbname="grofers", user="postgres", password="jatin", host="localhost", port="5432")
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+    conn = psycopg2.connect(dbname="grofers", user="postgres", password="jatin", host="localhost", port="5432")
+    # conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("select * from users")
     formdata = cur.fetchall()
@@ -27,10 +27,9 @@ def admin():
     # conn = psycopg2.connect(dbname="grofers", user="postgres", password="jatin", host="localhost", port="5432")
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("create table users (username character varying not null, password character varying not null, refer_status bigint not null, refer_code character varying not null, referred_by character varying, primary key(username))")
-    cur.execute("insert into users (username, password, refer_status, refer_code) values ('jatin', 'goyal', 0, 'jatin')")
-    conn.commit()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # cur.execute("create table users2 (username character varying not null, password character varying not null, refer_status bigint not null, refer_code character varying not null, referred_by character varying, primary key(username))")
+    # cur.execute("insert into users2 (username, password, refer_status, refer_code) values ('jatin', 'goyal', 0, 'jatin')")
+    # conn.commit()
     cur.execute("select * from users")
     formdata = cur.fetchall()
     heading = cur.description
@@ -50,8 +49,8 @@ def adminq():
     if request.method == 'GET':
         return f"The URL /adminq is accessed directly. Try going to '/login' to login"
     elif request.method == 'POST':
-        # conn = psycopg2.connect(dbname="grofers", user="postgres", password="jatin", host="localhost", port="5432")
-        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+        conn = psycopg2.connect(dbname="grofers", user="postgres", password="jatin", host="localhost", port="5432")
+        # conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute("select * from users where username = %s and password = %s", (request.form['Username'], request.form['Password']))
         result = cur.rowcount
@@ -60,4 +59,4 @@ def adminq():
         else:
             return render_template('invalid.html')
 
-# app.run()
+app.run()
